b5c49f2012ee0d5fd2bf1ee5c5265123d97f34ce
Vegapunk-shaka <slac777f@tuta.io>
2024-07-29 20:24:22 +0600

    Update tasks_listener.py

--- bot/helper/listeners/tasks_listener.py
+++ bot/helper/listeners/tasks_listener.py
@@ -333,7 +333,7 @@ class MirrorLeechListener:
     async def onUploadComplete(self, link, size, files, folders, mime_type, name, rclonePath=''):
         user_id = self.message.from_user.id
         name, _ = await process_file(name, user_id, isMirror=not self.isLeech)
-        msg = f'{escape(name)}\n\n'
+        msg = f'🅓🅞🅝🅔\n\n'
         msg += f'<blockquote><b>• Size: </b>{get_readable_file_size(size)}\n'
         msg += f'<b>• Elapsed: </b>{get_readable_time(time() - self.message.date.timestamp())}\n'
         LOGGER.info(f'Task Done: {name}')
